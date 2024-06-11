9aaaffde16be458abcd5cb7a287c2aa097c7fb4b
Michael Primavera <pastasauce59@gmail.com>
2024-06-11 17:53:04 -0400

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+import List from './components/List';
+
+function App(props) {
+
+  const [usrInput, setUsrInput] = useState("")
+
+  return (
+    <div className='main'>
+      <h1 className='title'>Task List CRUD Operations</h1>
+
+      <div className='input_holder'>
+        <input type="text" value={usrInput} onChange={(e) => setUsrInput(e.target.value)} />
+      </div>
+
+      <button className='button' type="submit">
+        Add Task!!!
+      </button>
+
+      <ul className='task-list'>
+        <List />
+      </ul>
+
+    </div>
+  );
+}
+
+export default App;
