37cfbbf77881adf896d3d508b29d78be42fb548d
Michael Primavera <pastasauce59@gmail.com>
2024-06-12 14:35:14 -0400

    Corrected two silly typo errors that were causing axios to yell errors at me :(

--- backend/controllers/TaskControllers.js
+++ backend/controllers/TaskControllers.js
@@ -2,7 +2,7 @@ const TaskModel = require("../models/TaskModel")
 
 module.exports.getTasks = async (req, res) => {
     const task = await TaskModel.find()
-    res.send(tasks)
+    res.send(task)
 // res.send("Hello from getTasks!")
 }
 
